f2d684e1144575a53f823474579bacb4ef73ade6
Zen Design Software <admin@zendesignsoftware.com>
2024-12-06 21:01:11 +0000

    fix: update more packages and resolve issue loading version

--- demo/src/app/versions.ts
+++ demo/src/app/versions.ts
@@ -1,8 +1,10 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-var */
 
 // extracts only the minor version from package.json
 // ex. ngBootstrap: "9.0.0" -> "9.0.0"
 // ex. bootstrap: "4.0.1" -> "4.0"
+declare var require: any;
 export const versions = {
 	bootstrap: `${require('../../../package.json').devDependencies['bootstrap'].split('.').slice(0, 2).join('.')}`,
 	ngBootstrap: `${require('../../../package.json').version}`,
